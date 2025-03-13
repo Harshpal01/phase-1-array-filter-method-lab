6ba3373689c467b82b83777c4c23f3bd2a2836d3
Dominic Kipkorir <harsh979pal@gamil.com>
2025-03-13 22:37:09 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+// findMatching(drivers, query)
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+
+  // fuzzyMatch(drivers, query)
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+  }
+
+  // matchName(drivers, query)
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name === query);
+  }
+  
